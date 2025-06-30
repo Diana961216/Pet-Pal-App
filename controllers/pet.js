19a3e0e3fb976483805fa954c70b44040ecbbe28
DianaArman1216 <dianmap96@icloud.com>
2025-06-29 21:16:02 -0400

    show, edit, update pets ready

--- controllers/pet.js
+++ controllers/pet.js
@@ -45,4 +45,50 @@ router.post('/', async (req, res) => {
     }
   });
 
+  router.get('/:petId', async (req, res) => {
+    try {
+      const pet = await Pet.findById(req.params.petId).populate('owner');
+      if (!pet) {
+        return res.status(404).send('Pet not found');
+      }
+      res.render('pets/show.ejs', { pet });
+    } catch (error) {
+      console.error(error);
+      res.redirect('/pets');
+    }
+  });
+
+  router.get('/:petId/edit', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        const pet = await Pet.findById(req.params.petId);
+        if (!pet) {
+          return res.status(404).send('Pet not found');
+        }
+        res.render('pets/edit.ejs', { pet, user: currentUser });
+      } catch (error) {
+        console.error(error);
+        res.redirect('/pets');
+      }
+  });
+
+  router.put('/:petId', async (req, res) => {
+    try {
+      const pet = await Pet.findByIdAndUpdate(req.params.petId, {
+        name: req.body.name,
+        breed: req.body.breed,
+        age: req.body.age,
+      }, { new: true });
+  
+      if (!pet) {
+        return res.status(404).send('Pet not found');
+      }
+  
+      res.redirect(`/pets/${pet._id}`);
+    } catch (error) {
+      console.error(error);
+      res.redirect('/pets');
+    }
+  });
+    
 module.exports = router;
